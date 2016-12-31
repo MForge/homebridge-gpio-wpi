1df3dd92a3957b9b37ddac265ba60fd9a375e86a
rsg98 <richard.grime@gmail.com>
2016-12-31 10:50:35 +0000

    Remove debug statements from readExports

--- readExports.js
+++ readExports.js
@@ -5,7 +5,6 @@ const SYSFS_GPIO = '/sys/class/gpio/gpio';
 
 module.exports = function() {
     var gpioexports = [];
-    console.log("Reading exports...");
     for(var i=0; i < MAX_GPIO; ++i){
       try {
           fs.accessSync(SYSFS_GPIO + i, fs.F_OK);
@@ -19,13 +18,11 @@ module.exports = function() {
             'value': value.trim()
           };
 
-          console.log(gpio);
           gpioexports.push(gpio);
 
       } catch (e) {
         if(e.code != 'ENOENT') {
           var gpio = { 'pin': i, 'error': e.code };
-          console.log(gpio);
           gpioexports.push(gpio);
         }
       }
